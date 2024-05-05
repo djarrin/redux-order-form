c89cbb4ba4c27534d522a4226dfbea8bcf1ebe40
David Jarrin <dmjarrin@gmail.com>
2024-05-05 18:41:13 -0400

    updating for new dependencies

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import reducer from './redux-resources/reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
